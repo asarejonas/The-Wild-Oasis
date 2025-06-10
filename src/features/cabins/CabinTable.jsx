854c996cf8efae74db18a12a6d95a8b1df937a3c
sabimbi447 <101404202+sabimbi447@users.noreply.github.com>
2025-06-10 12:59:19 +0100

    Hand form submition

--- src/features/cabins/CabinTable.jsx
+++ src/features/cabins/CabinTable.jsx
@@ -39,7 +39,6 @@ function CabinTable() {
     queryKey: ["cabins"],
     queryFn: getCabins,
   });
-  console.log(cabins);
   if (isLoading) return <Spinner />;
   if (error) return <div>Error: {error.message}</div>;
   if (!cabins) return null;
