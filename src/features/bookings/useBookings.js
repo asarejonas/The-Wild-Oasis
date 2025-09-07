7e3dc082279efb6a938935820b659cc0bb25d188
sabimbi447 <101404202+sabimbi447@users.noreply.github.com>
2025-09-07 15:41:38 +0100

    Booking hook

---
+++ src/features/bookings/useBookings.js
@@ -0,0 +1,34 @@
+/*eslint-disable */
+
+import { useQuery } from "@tanstack/react-query";
+import { getBookings } from "../../services/apiBookings";
+import { useSearchParams } from "react-router-dom";
+
+export function useBookings() {
+  const [searchParams] = useSearchParams();
+
+  // Filter
+  const filterValue = searchParams.get("status");
+
+  const filter =
+    !filterValue || filterValue === "all"
+      ? null
+      : { field: "status", value: filterValue };
+  // { field: "totalPrice", value: 500, method: "gte" };
+
+  ///SORT
+  const sortByRaw = searchParams.get("status") || "startDate-desc";
+  const [field, direction] = sortByRaw.split("-");
+  const sortBy = { field, direction };
+
+  const {
+    isLoading,
+    data: bookings,
+    error,
+  } = useQuery({
+    queryKey: ["bookings", filter, sortBy],
+    queryFn: () => getBookings({ filter, sortBy }),
+  });
+
+  return { isLoading, error, bookings };
+}
