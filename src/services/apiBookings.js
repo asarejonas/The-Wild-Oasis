80abdf05edada7983650e75d975c7009938c700b
sabimbi447 <101404202+sabimbi447@users.noreply.github.com>
2025-09-07 15:43:07 +0100

    Booking API

--- src/services/apiBookings.js
+++ src/services/apiBookings.js
@@ -1,8 +1,22 @@
+/*eslint-disable */
+
 import { getToday } from "../utils/helpers";
 import supabase from "./supabase";
 
-export async function getBookings() {
-  const { data, error } = await supabase.from("bookings").select("*");
+export async function getBookings({ filter, sortBy }) {
+  let query = supabase
+    .from("bookings")
+    .select(
+      "id, created_at, startDate, endDate, numNights, numGuests, status, totalPrice, cabins(name), guests(fullName, email)"
+    );
+
+  // Filter
+  if (filter) query = query[filter.method || "eq"](filter.field, filter.value);
+  const { data, error } = await query;
+
+  // SORT
+  if (sortBy)
+    query.order(sortBy.field, { ascending: sortBy.direction === "asc" });
 
   if (error) {
     console.error(error);
